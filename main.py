046c3fb16df05d7328a2558d17f05bdd270a682c
francomilanesioait <francomilanesioait@gmail.com>
2023-07-06 21:13:12 -0300

    ADD: function name change barra_de_carga() to loading_bar() + function creation charge_discount_code() + exercise N3 done

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ import requests
 import pandas as pd
 import time
 import os
+import random
 
 
-def barra_de_carga():
+def loading_bar(
+    total_time: int,
+    interval: float,
+    current_time: int,
+    clear: bool
+):
     """
     Funcion encargada de generar barra de carga en la interfaz
     """
-    total_time = 2  # Tiempo total en segundos
-    interval = 0.1  # Intervalo de actualización de la barra en segundos
-    current_time = 0
+    total_time = total_time  # Tiempo total en segundos
+    interval = interval  # Intervalo de actualización de la barra en segundos
+    current_time = current_time
 
     while current_time < total_time:
         current_time += interval
@@ -19,7 +25,8 @@ def barra_de_carga():
         print(f"Cargando: {percentage}% {bar}", end='\r')
         time.sleep(interval)
 
-    os.system('clear')
+    if clear:
+        os.system('clear')
 
 
 def is_temperature_greater_than_twenty_eight(
@@ -41,18 +48,69 @@ def is_temperature_greater_than_twenty_eight(
         return False
     
 
+def charge_discount_code(
+    product_name: str,
+    quantity: int
+) -> bool:
+    """
+    Funcion encargada de recibir el discount_code para poder validarlo
+    """
+    discount_code = input('\nIngrese el código de descuento: ')
+
+    print(f'\nValidando código: {discount_code} ingresado')
+
+    loading_bar(
+        total_time=2,
+        interval=0.1,
+        current_time=0,
+        clear=True
+    )
+
+    validate_discount = validate_discount_code(
+        discount_code=discount_code
+    )
+    
+    if validate_discount:
+        print(f'\nConfirmando el pedido del producto: {product_name} cantidad: {quantity}\n')
+
+        loading_bar(
+            total_time=3,
+            interval=0.1,
+            current_time=0,
+            clear=False
+        )
+
+        print(f'Pedido numero: {random.randint(0, 10000)} confirmado con exito!')
+     
+    else:
+        print(f"Codigo de descuento: {discount_code} no es valido\n")
+
+        charge_discount_code(
+            product_name=product_name,
+            quantity=quantity
+        )
+  
+
 def charge_product_name_and_quantity():
     """
     Funcion encargada de pedir el nombre y la cantidad del producto
     asi como pasarle los parametro product_name y quantity a la funcion
     is_product_available()
-
-    :return False `bool`si is_product_available retorna False:
-    :return True `bool`si is_product_available retorna True:
     """
     product_name = input('\nIngrese el nombre del producto: ')
 
     quantity = int(input("Ingrese cantidad: "))
+
+    print(f"\nBuscando producto: {product_name} cantidad: {quantity}")
+
+    loading_bar(
+        total_time=2,
+        interval=0.1,
+        current_time=0,
+        clear=True
+    )
+
+    time.sleep(1)
 
     get_product_name_and_quantity_from_df()
 
@@ -62,8 +120,16 @@ def charge_product_name_and_quantity():
     )
 
     if results:
-        print("\nSe encontro Stock del producto: ", product_name)
+        print("\nSe encontro Stock del producto:",  product_name)
+
+        charge_discount_code(
+            product_name=product_name,
+            quantity=quantity
+        ) 
+
     else:
+        print("\nNo se encontro stock del producto:",  product_name)
+
         charge_product_name_and_quantity()
     
 
@@ -123,10 +189,16 @@ def get_product_name_and_quantity_from_df():
     return print("\n", board)
 
 
-def is_product_available(product_name, quantity):
+def is_product_available(
+    product_name: str, 
+    quantity: int
+) -> bool:
     """
     Funcion encargada de: Buscar en un pandas DataFrame y devolver True si existe stock, False caso
     contrario.
+
+    :return False `bool`si quantity es menos o igual a 0 o mayor al stock disponible o el stock es 0 o menor:
+    :return True `bool`si queantity es mayor a cero, menor o igual al stock disponible y el stock disponible es mayor a cero:
     """
     search = PRODUCT_DF.loc[PRODUCT_DF["product_name"] == product_name, "quantity"]
 
@@ -141,17 +213,57 @@ def is_product_available(product_name, quantity):
 
 
 # Ejercicio 3:
+_AVAILABLE_DISCOUNT_CODES = ["Primavera2021", "Verano2021", "Navidad2x1", "heladoFrozen"]
 
+
+def validate_discount_code(
+    discount_code: str
+) -> bool:
+    """
+    Dada la lista de códigos de descuento vigentes y un código de descuento
+    mencionado por el cliente, devuelve True si la diferencia entre el código
+    mencionado y los códigos vigentes es menor a tres caracteres, en al menos
+    uno de los casos.
+
+    :param discount_code: Código de descuento mencionado por el cliente
+    :return: True si hay una diferencia de menos de tres caracteres con al menos uno de los códigos vigentes, False de lo contrario
+
+    Ejemplo:
+    "primavera2021" deberia devolver True, ya que al compararlo:
+    vs "Primavera2021" = 2 caracteres de diferencia ("p" y "P")
+    vs "Verano2021" = 7 caracteres de diferencia ('i', 'n', 'o',
+    'm', 'V', 'p', 'v')
+    vs "Navidad2x1" = 8 caracteres de diferencia ('N', 'm', '0',
+    'x', 'e', 'd', 'p', 'r')
+    vs "heladoFrozen" = 14 caracteres de diferencia ('z', 'i',
+    'v', 'n',
+    'o', 'm', '2', '0', 'd', 'p', '1', 'F', 'h', 'l')
+    """
+    for code in _AVAILABLE_DISCOUNT_CODES:
+        differences = sum(c1 != c2 for c1, c2 in zip(discount_code, code))
+        if differences < 3:
+            return True
+    
+    return False
 
 def start():
     """
     Funcion encargada de iniciar el bot
     """
-    print("Iniciando Bot.......")
-    time.sleep(5.5)
+    os.system('clear')
+
+    print("Iniciando Bot.......\n")
+    time.sleep(3.5)
     
     print("Buscando temperatura en: pehuajo")
-    barra_de_carga()
+    time.sleep(2)
+
+    loading_bar(
+        total_time=2,
+        interval=0.1,
+        current_time=0,
+        clear=True
+    )
 
     if GeoAPI.is_hot_in_pehuajo():
         print("#" * 18)
